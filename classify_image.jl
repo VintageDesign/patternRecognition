ad2ee2f1e047bc337489635c092aaa3943f3dfd6
Riley Kopp <koppril7@gmail.com>
2019-12-03 18:53:23 -0700

    Added the script with most of the function calls

---
+++ classify_image.jl
@@ -0,0 +1,33 @@
+using TestImages
+# Select A Random Image -- Caelan
+testImagePath = selectImage()
+
+
+
+U, sigma, imageVector, meanA, X = generateBadManifold()
+
+
+k = computeER(imageVector, sigma, .95)
+
+
+for i = 1:k
+    M[i,:] = X * U[:, i]
+end
+
+
+testImage = mat(testimage(testImagePath))
+
+
+testImageVec = vec(testImage)
+
+
+testImageMeanCentered = testImageVec - meanA
+
+for i = 1:k
+    t[i,:] = testImageMeanCentered * U[:, i]
+end
+
+
+#KNN search
+
+#Show Image and object
