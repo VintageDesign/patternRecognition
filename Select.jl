1ebf3e512607407a270081c7a27bbd00599c2c0d
Riley Kopp <koppril7@gmail.com>
2019-12-05 17:43:40 -0700

    Dumping changes

--- Select.jl
+++ Select.jl
@@ -3,12 +3,13 @@
 Description: Randomly selects the path to an image in the \TestImages\UnProcessed
              folder. The path to that image is returned.
 =#
-module selectImage
+module Select
 
-    function select()
+    function selectImage()
 
-        testImages = dirname(@__FILE__) * "\\TestImages\\"
-        imageDir = testImages * rand(readdir(testImages)) * "\\UnProcessed\\"
+        # / for Linux \\ for windows files
+        testImages = dirname(@__FILE__) * "/TestImages/"
+        imageDir = testImages * rand(readdir(testImages)) * "/UnProcessed/"
 
         return imageDir * rand(readdir(imageDir))
     end
