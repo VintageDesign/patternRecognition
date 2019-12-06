fd0e5f63a7572896bad791e410b98cd1e7d1eac7
Campbell, Riley <riley.campbell@mines.sdsmt.edu>
2019-12-05 22:19:07 -0700

    send it

--- Load.jl
+++ Load.jl
@@ -28,4 +28,42 @@ module Load
         end
         return vec(image_mean ./ 128*20), imageVectors
     end
+
+function loadSingleImage()
+
+        image_mean = zeros(Float64, 128, 128)
+        k = 1
+        # assume images are grayscale and in /images directory
+        # filenames are img_#.png
+        imageVectors = Array{Gray{Float64}, 1}[]
+        imgFileNames = String[]
+        #generate image file names
+        println("Generating File Names")
+        for x = 0:127
+            fName = "TrainingImages/Boat64/UnProcessed/img_$x.png"
+
+            push!(imgFileNames, fName)
+        end
+        println("File Names Generated...\n")
+
+        # load images
+        println("Loading images...")
+        p = Progress(length(imgFileNames), 1)
+
+        for img = imgFileNames
+            # println("Open image: $img")
+            img = load(img) # load image
+            img = float64.(img) # convert to float
+            image_mean = image_mean .+ img
+            vecImg = vec(img)
+            push!(imageVectors, vecImg)
+            next!(p)
+        end
+        println("Images Loaded...\n")
+
+        println("Getting Image Mean")
+        image_mean = vec(image_mean ./ 64)
+        return image_mean, imageVectors
+end
+   export loadSingleImage, loadAllImages
 end
--- SingleManifold.jl
+++ SingleManifold.jl
@@ -5,7 +5,7 @@ Description: File that holds a function to generate a single manifold
 
 module SingleManifold
 
-using Images, ImageInTerminal, LinearAlgebra, ProgressMeter
+using Images, ImageInTerminal, LinearAlgebra, ProgressMeter, Load
 
 #=
      Author: Timothy Ford
@@ -13,38 +13,7 @@ Description: Function that generates reduces the dimensionality of a training ob
             and returns the necessary values from and SVD.
 =#
     function generateSingleManifold(itemName)
-        image_mean = zeros(Float64, 128, 128)
-        k = 1
-        # assume images are grayscale and in /images directory
-        # filenames are img_#.png
-        imageVectors = Array{Gray{Float64}, 1}[]
-        imgFileNames = String[]
-        #generate image file names
-        println("Generating File Names")
-        for x = 0:127
-            fName = "TrainingImages/" * itemName * "64/UnProcessed/img_$x.png"
-            push!(imgFileNames, fName)
-        end
-        println("File Names Generated...\n")
-
-        # load images
-        println("Loading images...")
-        p = Progress(length(imgFileNames), 1)
-
-        for img = imgFileNames
-            # println("Open image: $img")
-            img = load(img) # load image
-            img = float64.(img) # convert to float
-            image_mean = image_mean .+ img
-            vecImg = vec(img)
-            push!(imageVectors, vecImg)
-            next!(p)
-        end
-        println("Images Loaded...\n")
-
-        println("Getting Image Mean")
-        image_mean = vec(image_mean ./ 64)
-
+        image_mean, imageVectors = loadSingleImage()
         # Image matrix (2D array instead of array of arrays)
         imageMatrix = permutedims(reshape(hcat(imageVectors...), (length(imageVectors[1]), length(imageVectors))))
 
